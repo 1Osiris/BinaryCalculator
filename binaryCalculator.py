23d1782dc8bb75235c650d770fed20e26e616c66
Julian <julmatthews24@gmail.com>
2022-12-25 00:51:51 -0500

    Changed function name to be more specififc

--- binaryCalculator.py
+++ binaryCalculator.py
@@ -2,7 +2,7 @@
 # formula for Binary --> Decimal
 
 import math
-def binaryCalc(binary: int, count: int = 0):
+def binaryCalcInt(binary: int, count: int = 0):
     try:
         count = 0 # acts the exponent that 2 is raised to
         sum = 0 # will be used as the central sum of all the calculations
@@ -17,11 +17,11 @@ def binaryCalc(binary: int, count: int = 0):
     except: 
         return "the input was not an integer or was not only 1s and 0s" # catches other errors like TypeErrors
 
-print(binaryCalc(0)) # 0 -BinaryToDecimal-> 0
-print(binaryCalc(1)) # 1 -BinaryToDecimal-> 1
-print(binaryCalc(10)) # 10 -BinaryToDecimal-> 3
-print(binaryCalc(1010)) # 1010 -BinaryToDecimal-> 100
-print(binaryCalc(1100100)) # 1100100 -BinaryToDecimal-> 1000
-print(binaryCalc(11100010)) # 11100010n -BinaryToDecimal-> 226
-print(binaryCalc(13)) # 13 -BinaryToDecimal-> Invalid input, Error Thrown, because 13 is not a valid binary number
-print(binaryCalc("1010")) # "1010" -BinaryToDecimal-> Invalid input, Error Thrown, because String is not a valid input type
+print(binaryCalcInt(0)) # 0 -BinaryToDecimal-> 0
+print(binaryCalcInt(1)) # 1 -BinaryToDecimal-> 1
+print(binaryCalcInt(10)) # 10 -BinaryToDecimal-> 3
+print(binaryCalcInt(1010)) # 1010 -BinaryToDecimal-> 100
+print(binaryCalcInt(1100100)) # 1100100 -BinaryToDecimal-> 1000
+print(binaryCalcInt(11100010)) # 11100010n -BinaryToDecimal-> 226
+print(binaryCalcInt(13)) # 13 -BinaryToDecimal-> Invalid input, Error Thrown, because 13 is not a valid binary number
+print(binaryCalcInt("1010")) # "1010" -BinaryToDecimal-> Invalid input, Error Thrown, because String is not a valid input type
